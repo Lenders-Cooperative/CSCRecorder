734cd224ef85b49f4243f3833849d122f7eb87ef
Stephen Lightcap <stephenlightcap121212@gmail.com>
2021-10-12 00:45:31 -0500

    updated requests to a base class, more routes, paper params

--- csc_recorder/CSCRecorder.py
+++ csc_recorder/CSCRecorder.py
@@ -1,9 +1,11 @@
+from typing import OrderedDict, Tuple
 import requests
 from jinja2 import (
     Environment,
     PackageLoader,
     select_autoescape
 )
+from csc_recorder.APIHandler import APIHandler
 import xmltodict
 import json
 
@@ -13,24 +15,23 @@ env = Environment(
     autoescape=select_autoescape()
 )
 
-class CSCRecorder():
-    TEST_URL = 'https://ep4-uat.erecording.com/api/'
+class CSCRecorder(APIHandler):
+    TEST_URL = 'https://ep4-uat.erecording.com/api'
     PROD_URL = 'http://'
-    REQUEST_TIMEOUT = 20
-    headers = {
-        'Content-Type': 'application/xml'
-    }
 
     def __init__(self, username, password, test=False):
-        if not test:
-            self.url = self.TEST_URL
-        else:
-            self.url = self.PROD_URL
+        self.host = self.TEST_URL if not test else self.PROD_URL
 
-        self.username = username
-        self.password = password
+        self.api_handler = APIHandler(
+            self.host, 
+            username,
+            password,
+            headers={
+                'Content-Type': 'application/xml'
+            }
+        )
 
-    def _clean_response(self, r):
+    def _clean_response(self, r: str) -> str:
         r = r.encode('ascii', 'ignore').decode('unicode_escape')
         r = r.replace('<?xml version="1.0" encoding="UTF-8"?>', '')
         r = r[1:]
@@ -38,7 +39,7 @@ class CSCRecorder():
         return r
 
     def _parse_xml_string(self, xml):
-        return None
+        ...
     
     def _get_fips(self):
         ...
@@ -47,9 +48,11 @@ class CSCRecorder():
         self,
         client_package_id: str,
         fips: int,
+        assigned_office: str,
         params: dict = {},
-        no_document=False
-    ) -> dict:
+        service_type: str = None,
+        no_document: bool = False,
+    ) -> Tuple[OrderedDict, requests.Response]:
         """
         Sends a request to generate a package to CSC eRecorder
         
@@ -59,7 +62,11 @@ class CSCRecorder():
             "document_type": "Deed",
         }
 
+        :param client_package_id: the package ID that we will be uploading
+        :param fips: the FIPS code of the county
+        :param assigned_office: the name of the office to assign the package to
         :param params: dict of parameters to send to CSC
+        :param service_type: currently only 'paperfulfillment' option 
         :param no_document: bool, if true, will send no document info
         :return: dict of package information
         """
@@ -67,67 +74,88 @@ class CSCRecorder():
         params['no_document'] = no_document
         params['client_package_id'] = client_package_id
         params['fips'] = fips # TODO generate FIPS
+        params['assigned_office'] = assigned_office
         payload = template.render(**params)
 
-        response = requests.request(
-            "POST", 
-            f"{self.url}/v1/package?contentType=xml", 
-            headers=self.headers, 
-            timeout=self.REQUEST_TIMEOUT,
-            data=payload,
-            auth=(self.username, self.password)
-        )
+        print(payload)
 
-        if response.status_code not in [200, 201]:
-            return None
+        url = "/v1/package?contentType=xml"
+        if service_type: url += f"&serviceType={service_type}"
+
+        response = self.api_handler.send_request(
+            "POST", url, payload
+        )
 
         cleaned_response = self._clean_response(str(response.text))
         
-        return xmltodict.parse(cleaned_response)
+        return xmltodict.parse(cleaned_response), response
     
-    def get_document_type(self, fips: dict):
-        url = f"{self.url}/v1/documentType/{fips}"
-        response = requests.request(
-            "GET", 
-            url, 
-            headers=self.headers,
-            timeout=self.REQUEST_TIMEOUT,
-            auth=(self.username, self.password)
+    def get_document_type(self, fips: str) -> Tuple[dict, requests.Response]:
+        response = self.api_handler.send_request(
+            "GET", f"/v1/documentType/{fips}"
+        )
+        
+        return json.loads(response.content), response
+    
+    def get_package_status(self, binder_id: str) -> Tuple[dict, requests.Response]:
+        """
+        Returns a packages status with file and fees.
+
+        :param binder_id: the binder ID of the package
+        """
+        response = self.api_handler.send_request(
+            "GET",
+            (f"/v3/package/{binder_id}?returnFileType=pdf"
+            "&embed=true&contentType=json&includeImage=true")
         )
 
-        if response.status_code not in [200, 201]:
-            return None
+        return xmltodict.parse(response.content), response
+    
+    def get_mortgage_tax_req(self, county_id: str) -> Tuple[dict, requests.Response]:
+        """
         
-        return json.loads(response.content)
-    
-    def get_package_status(self, binder_id: str):
-        url = (f"{self.url}/v3/package/{binder_id}?returnFileType"
-                "=pdf&embed=true&contentType=json&includeImage=true")
-        response = requests.request(
-            "GET", 
-            url,
-            headers=self.headers,
-            timeout=self.REQUEST_TIMEOUT,
-            auth=(self.username, self.password)
+        """
+        response = self.api_handler.send_request(
+            "GET",
+            (f"/v1/county/{county_id}"
+            "/AdditionalMortgageTaxNoRecordingFee/requirements")
         )
+        return json.loads(response.content), response
     
-        if response.status_code not in [200, 201]:
-            return None
-    
-        return xmltodict.parse(response.content)
-    
-    def additonal_mortgage_tax_requirements(self, county_id: str):
-        url = (f"{self.url}/v1/county/{county_id}"
-                "/AdditionalMortgageTaxNoRecordingFee/requirements")
-        response = requests.request(
-            "GET", 
-            url, 
-            headers=self.headers,
-            timeout=self.REQUEST_TIMEOUT,
-            auth=(self.username, self.password)
+    def get_assigned_office(self) -> Tuple[dict, requests.Response]:
+        """
+        Returns the current selected office from the CSC Platform.
+        """
+        response = self.api_handler.send_request(
+            "GET",
+            "/v1/office/assigned"
         )
 
-        if response.status_code not in [200, 201]:
-            return None
-        
-        return json.loads(response.content)
+        return json.loads(response.content), response
+    
+    def get_offices(self) -> Tuple[dict, requests.Response]:
+        """
+        Returns all offices that have been created for your account.
+        """
+        response = self.api_handler.send_request(
+            "GET",
+            "/v1/office"
+        )
+
+        return json.loads(response.content), response
+    
+    def get_state_offices(self, state: str, service_type=None) -> Tuple[dict, requests.Response]:
+        """
+        Returns a list of county offices for that given state.
+
+        :param state: the state to get county offices for
+        :param service_type: the service type to filter by Ex: 'paper'
+        """
+        url = f"/v1/state/{state}"
+        if service_type: url += f"?serviceType={service_type}" 
+
+        response = self.api_handler.send_request(
+            "GET", url
+        )
+
+        return json.loads(response.content), response
