06df0c72bc8dc9a8500c7d3e31b731e64f489b73
Josh Dennis <jdennis@thesummitgrp.com>
2022-10-20 09:37:49 -0500

    update disable_existing_loggers and version bump

--- csc_recorder/constants.py
+++ csc_recorder/constants.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent
 
 LOGGING_CONFIG = {
     "version": 1,
-    "disable_existing_loggers": True,
+    "disable_existing_loggers": False,
     "formatters": {
         "development": {
             "format": (
